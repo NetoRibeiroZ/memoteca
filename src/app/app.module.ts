1a43531926e5fcecc3e4030c833afe4953deac90
Neto Rodrigues <antoniodrigues@hotmail.com>
2023-03-19 13:17:34 -0300

    adding forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { FormularioComponent } from './componentes/formulario/formulario.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabecalhoComponent
+    CabecalhoComponent,
+    RodapeComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/formulario/formulario.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent implements OnInit {
+
+pensamento ={
+  id: '1',
+  conteudo:'Aprendendo Angular',
+  autoria:'Dev',
+  modelo:'modelo1',
+  value:''
+}
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+  criarPensamento(){
+    alert('novo pensamento criado');
+  }
+  cancelar(){
+    alert('pensamento cancelado');
+  }
+}
